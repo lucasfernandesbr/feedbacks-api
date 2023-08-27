84ff301e8a6a0342972bbc65b09f8fda16965a96
Lucas <lucas.fernandes@gympass.com>
2023-08-27 12:06:53 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,14 @@
 import fastify from 'fastify'
 
+import cors from '@fastify/cors'
+
 export const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+  methods: ['GET'],
+})
+
+app.get('/', () => ({
+  ok: true,
+}))
