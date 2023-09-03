d377eb3555e02d9ca62ccb91a7f5b09d22185c93
Lucas Souza <ls79297@gmail.com>
2023-09-03 19:59:55 -0300

    adjust authenticate flow

--- src/use-cases/cases/authenticate/index.ts
+++ src/use-cases/cases/authenticate/index.ts
@@ -17,7 +17,32 @@ export class Authenticate {
     const doesUserExist = await this.usersRepository.findByUsername(username)
 
     if (!doesUserExist) {
-      throw new CaseError('User not found.', 404)
+      const getGithubData: GithubUser = await api.get<GithubUser, GithubUser>(
+        `https://api.github.com/users/${username}`,
+      )
+
+      if (!getGithubData) {
+        throw new CaseError('Github user not found.', 404)
+      }
+
+      const { name, avatar_url, bio } = getGithubData.data
+      const passwordHash = await hash(password, 8)
+
+      const user = await this.usersRepository.create({
+        name,
+        username,
+        password: passwordHash,
+        avatar_url,
+        bio,
+      })
+
+      return {
+        name: user.name,
+        username: user.username,
+        avatar_url: user.avatar_url,
+        bio: user.bio,
+        created_at: user.created_at,
+      }
     }
 
     const passwordCompare = await compare(password, doesUserExist.password)
@@ -26,48 +51,19 @@ export class Authenticate {
       throw new CaseError('Incorrect password.', 401)
     }
 
-    if (doesUserExist && passwordCompare) {
-      const feedbacks = doesUserExist.feedbacks.map(
-        ({ title, content, type, created_at }) => {
-          return { title, content, type, created_at }
-        },
-      )
-
-      return {
-        name: doesUserExist.name,
-        username: doesUserExist.username,
-        avatar_url: doesUserExist.avatar_url,
-        bio: doesUserExist.bio,
-        created_at: doesUserExist.created_at,
-        feedbacks,
-      }
-    }
-
-    const getGithubData: GithubUser = await api.get<GithubUser, GithubUser>(
-      `https://api.github.com/users/${username}`,
+    const feedbacks = doesUserExist.feedbacks.map(
+      ({ title, content, type, created_at }) => {
+        return { title, content, type, created_at }
+      },
     )
 
-    if (!getGithubData) {
-      throw new CaseError('Github user not found.', 404)
-    }
-
-    const { name, avatar_url, bio } = getGithubData.data
-    const passwordHash = await hash(password, 8)
-
-    const user = await this.usersRepository.create({
-      name,
-      username,
-      password: passwordHash,
-      avatar_url,
-      bio,
-    })
-
     return {
-      name: user.name,
-      username: user.username,
-      avatar_url: user.avatar_url,
-      bio: user.bio,
-      created_at: user.created_at,
+      name: doesUserExist.name,
+      username: doesUserExist.username,
+      avatar_url: doesUserExist.avatar_url,
+      bio: doesUserExist.bio,
+      created_at: doesUserExist.created_at,
+      feedbacks,
     }
   }
 }
